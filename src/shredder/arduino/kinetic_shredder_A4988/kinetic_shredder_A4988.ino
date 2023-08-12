87e49dc393bc4ed2c263bfe8b79bc2ed996bcf49
nub340 <nkoier@gmail.com>
2023-08-12 16:19:51 -0700

    organize

--- src/shredder/arduino/kinetic_shredder_A4988/kinetic_shredder_A4988.ino
+++ src/shredder/arduino/kinetic_shredder_A4988/kinetic_shredder_A4988.ino
@@ -35,9 +35,6 @@ void setup() {
   Serial.begin(9600);
   while (!Serial); //wait until ready
 
-//  setShreddedState(false);
-//  setMutedState(false);
-
   isShredded = getShreddedState();
   isMuted = getMutedState();
   location = getLocationState();
