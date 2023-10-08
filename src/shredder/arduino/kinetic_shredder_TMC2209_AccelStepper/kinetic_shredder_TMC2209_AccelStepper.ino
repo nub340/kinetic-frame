836195ec9f0f15e03fbfa0ead2300be4dda1a884
nub340 <nkoier@gmail.com>
2023-10-07 18:15:21 -0700

    stepper motor impls

--- src/shredder/arduino/kinetic_shredder_TMC2209_AccelStepper/kinetic_shredder_TMC2209_AccelStepper.ino
+++ src/shredder/arduino/kinetic_shredder_TMC2209_AccelStepper/kinetic_shredder_TMC2209_AccelStepper.ino
@@ -1,11 +1,10 @@
 #include <AccelStepper.h>
 #include <IRremote.hpp>
 #include <EEPROM.h>
-#include <SoftwareSerial.h>
 
 #define EEPROM_ADDRESS 0
 
-#define BUZZ_PIN 2
+#define BUZZER_PIN 2
 #define CLK_PIN 3
 #define STEP_PIN 4
 #define DIR_PIN 5
@@ -29,37 +28,34 @@
 #define SPEED_DELAY 5000
 #define TOTAL_REVS 2.6
 #define TOTAL_STEPS (STEPS_PER_REV * TOTAL_REVS) * 8
-
-#define DRIVER_ADDRESS   0b00   // TMC2209 Driver address according to MS1 and MS2
-#define R_SENSE 0.11f           // SilentStepStick series use 0.11 ...and so does my fysetc TMC2209 (?)
+#define TRIM_STEPS 100 
 
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
-
 IRrecv irrecv(IR_PIN);
 
-long lastBuzzerMillis = 0;
-int buzzerState = LOW;
-bool isShredded = false;
-bool isMuted = false;
-int location = 0;
+long _lastBuzzerMillis = 0;
+long _irrecvPausedAtMillis = 0;
+int _buzzerState = LOW;
+bool _isShredded = false;
+bool _isMuted = false;
+int _location = 0;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial); //wait until ready
 
-  pinMode(BUZZ_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
-  // pinMode(DIR_PIN, OUTPUT);
-
+  pinMode(DIR_PIN, OUTPUT);
   pinMode(EN_PIN, OUTPUT);
-  digitalWrite(EN_PIN, HIGH);
+  digitalWrite(EN_PIN, LOW);      // LOW means enable
 
-  stepper.setMaxSpeed(500);       // Set the maximum speed in steps per second
-  stepper.setAcceleration(1000);   // Set the acceleration in steps per second^2
+  stepper.setMaxSpeed(1000);       // Set the maximum speed in steps per second
+  stepper.setSpeed(600);
+  stepper.setAcceleration(2000);  // Set the acceleration in steps per second^2
   
   IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
   
-  delay(250); // seems to mitigate unwanted motor movement
   Serial.println("running...");
   writeStateToSerial();
 }
@@ -85,12 +81,12 @@ void handleIRRemoteCommand(int command) {
   switch (command) {
     case IR_BUTTON_UP:
       Serial.println("Trim up");
-      stepReverse(10, false);
+      stepReverse(TRIM_STEPS, false);
       break;
 
     case IR_BUTTON_DOWN:
       Serial.println("Trim down");
-      stepForward(10, false);
+      stepForward(TRIM_STEPS, false);
       break;
 
     case IR_BUTTON_OK:
@@ -100,7 +96,7 @@ void handleIRRemoteCommand(int command) {
 
     case IR_BUTTON_ASTERISK:
       Serial.println("Reset location");
-      location = 0;
+      resetLocationState();
       break;
 
     case IR_BUTTON_LEFT:
@@ -116,16 +112,13 @@ void handleIRRemoteCommand(int command) {
       break;
   }
 }
-
 void runMainSequence() {
   Serial.println("runMainSequence");
-  
-  // Keep track of how long the IR receiver has been stopped
-  int startMillis = millis();
-  IrReceiver.stop();
+
+  stopIRReceiver();
 
   // If already in the shredded state, run the main sequence in reverse
-  bool isSequenceReversed = isShredded;
+  bool isSequenceReversed = _isShredded;
   
   if(isSequenceReversed){
     stepReverse(TOTAL_STEPS, false);
@@ -150,112 +143,116 @@ void runMainSequence() {
   delay(3000);
   Serial.println("Running...");
 
-  // Calculate how long the timer has been stopped. 
-  // Start IR receiver again and pass the elapsed time to avoid bug with internal clock.
-  int elapsedMicros = (millis() - startMillis) * 1000;
-  IrReceiver.start(elapsedMicros);
+  startIRReceiver();
 }
 
 void toggleMute() {
   Serial.println("Toggling mute");
-  setMutedState(!isMuted);
-  int startMillis = millis();
-  //must stop the IRReceiver as the tone/notone functions need to use the same timer
-  IrReceiver.stop(); 
-  tone(BUZZ_PIN, 3000);
+  setMutedState(!_isMuted);
+  stopIRReceiver();
+  tone(BUZZER_PIN, 3000);
   delay(500);
-  noTone(BUZZ_PIN);
-  int elapsedMicros = (millis() - startMillis) * 1000;
-  //keep track of how much time has elapsed and use it to restart the IRReceiver
-  IrReceiver.start(elapsedMicros); 
+  noTone(BUZZER_PIN);
+  startIRReceiver();
+}
+
+
+void resetLocationState() {
+  _location = 0;
 }
 
 void enterShreddedState() {
-  if (isShredded) {
+  if (_isShredded) {
     Serial.println("Already shredded");
     return;
   }
   Serial.println("Shredding...");
-  int startMillis = millis();
-  IrReceiver.stop();
+  stopIRReceiver();
   stepForward(TOTAL_STEPS, true);
   setShreddedState(true);
-  int elapsedMicros = (millis() - startMillis) * 1000;
-  IrReceiver.start(elapsedMicros);
+  startIRReceiver();
 }
 
 void enterNormalState() {
-  if (!isShredded) {
+  if (!_isShredded) {
     Serial.println("Already restored");
     return;
   }
   Serial.println("Restoring...");
-  int startMillis = millis();
-  IrReceiver.stop();
+  stopIRReceiver();
   stepReverse(TOTAL_STEPS, false);
   setShreddedState(false);
-  int elapsedMicros = (millis() - startMillis) * 1000;
-  IrReceiver.start(elapsedMicros);
-  location = 0;
+  startIRReceiver();
 }
-
 void stepForward(int steps, bool playBuzzer) {
   Serial.print("stepForward(");
   Serial.print(steps);
   Serial.println(")");
-  digitalWrite(EN_PIN, LOW);
-  //digitalWrite(DIR_PIN, HIGH);
+  //digitalWrite(DIR_PIN, LOW);
   delay(1);
 
-  stepper.move(0-steps);
-  while (stepper.distanceToGo() != 0) {
-    stepper.run();
+  for (int i = 0; i <= steps; i++) {
     if (playBuzzer) {
       handleBuzzer();
     }
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(SPEED_DELAY);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(SPEED_DELAY);
   }
-
-  setLocationState(location+steps);
-  digitalWrite(EN_PIN, HIGH);
-  noTone(BUZZ_PIN);
+  setLocationState(_location+steps);
+  noTone(BUZZER_PIN);
 }
 
 void stepReverse(int steps, bool playBuzzer) {
   Serial.print("stepReverse(");
   Serial.print(steps);
   Serial.println(")");
-  digitalWrite(EN_PIN, LOW);
-  //digitalWrite(DIR_PIN, LOW);
+  //digitalWrite(DIR_PIN, HIGH);
   delay(1);
 
-  stepper.move(steps);
-  while (stepper.distanceToGo() != 0) {
-    stepper.run();
+  for (int i = 0; i <= steps; i++) {
     if (playBuzzer) {
       handleBuzzer();
     }
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(SPEED_DELAY);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(SPEED_DELAY);
   }
+  setLocationState(_location-steps);
+  noTone(BUZZER_PIN);
+}
 
-  setLocationState(location-steps);
-  digitalWrite(EN_PIN, HIGH);
-  noTone(BUZZ_PIN);
+void stopIRReceiver() {
+  // capture millis when reciever was stopped.
+  _irrecvPausedAtMillis = millis();
+  IrReceiver.stop();
+}
+
+void startIRReceiver() {
+  // Calculate the elapsed millis since the IRReceiver was stopped. 
+  // Start IR receiver again and pass the elapsed time to avoid bug with internal clock.
+  // This took me weeks to figure out. The remote would only work once then a reboot was needed.
+  int elapsedMicros = (millis() - _irrecvPausedAtMillis) * 1000;
+  IrReceiver.start(elapsedMicros);
 }
 
 void handleBuzzer() {
-  if(isMuted) {
-    buzzerState = LOW;
-    noTone(BUZZ_PIN);
+  if(_isMuted) {
+    _buzzerState = LOW;
+    noTone(BUZZER_PIN);
   } else {
     unsigned long currentMillis = millis();
-    if (currentMillis - lastBuzzerMillis > 250) {
-      lastBuzzerMillis = currentMillis;
-      if (buzzerState == LOW) {
-        buzzerState = HIGH;
-        tone(BUZZ_PIN, 3000);
+    if (currentMillis - _lastBuzzerMillis > 250) {
+      _lastBuzzerMillis = currentMillis;
+      if (_buzzerState == LOW) {
+        _buzzerState = HIGH;
+        tone(BUZZER_PIN, 3000);
       }
       else {
-        buzzerState = LOW;
-        noTone(BUZZ_PIN);
+        _buzzerState = LOW;
+        noTone(BUZZER_PIN);
       }
     }
   }
@@ -263,40 +260,46 @@ void handleBuzzer() {
 
 void writeStateToSerial() {
   Serial.print("Muted: ");
-  Serial.print(isMuted);
+  Serial.print(_isMuted);
   Serial.print(", Shredded: ");
-  Serial.print(isShredded);
+  Serial.print(_isShredded);
   Serial.print(", Location: ");
-  Serial.print(location);
+  Serial.print(_location);
   Serial.println("");
 }
 
 void setShreddedState(bool shredded) {
+  _isShredded = shredded;
   EEPROM.put(EEPROM_ADDRESS, shredded);
 }
 
 bool getShreddedState() {
   bool shredded;
   EEPROM.get(EEPROM_ADDRESS, shredded);
+  _isShredded = shredded;
   return shredded;
 }
 
 void setMutedState(bool isMuted) {
+  _isMuted = isMuted;
   EEPROM.put(EEPROM_ADDRESS+1, isMuted);
 }
 
 bool getMutedState() {
   bool isMuted;
   EEPROM.get(EEPROM_ADDRESS+1, isMuted);
+  _isMuted = isMuted;
   return isMuted;
 }
 
 void setLocationState(int location) {
+  _location = location;
   EEPROM.put(EEPROM_ADDRESS+2, location);
 }
 
 int getLocationState() {
   int location;
   EEPROM.get(EEPROM_ADDRESS+2, location);
+  _location = location;
   return location;
 }
